331a870b228f4ff881c67d1084d1c9abcac747c6
kion1491 <kion1491@gmail.com>
2019-12-11 18:31:24 +0900

    코드 수정중
    코드 수정중

--- CSS/몬디의하루/javascript/main.js
+++ CSS/몬디의하루/javascript/main.js
@@ -1,27 +1,47 @@
 $(document).ready(function () {
   setTimeout(function () { $('.chatbotArea').animate({ opacity: '1' }, 1500); }, 1000)
+  
+  var mentNum = $(".ment_wave p").length;
+  console.log(mentNum);
 
-  var mentArray = [
-                    ["ment_wave_01(sub_ment_01)", "ment_wave_01(sub_ment_02)", "ment_wave_01(main_ment!!!!!!!!!!!!!!!!)"],
-                    ["ment_wave_02(sub_ment_01)", "ment_wave_02(sub_ment_02)", "ment_wave_02(main_ment!!!!!!!!!!!!!!!!)"],
-                    ["ment_wave_03(sub_ment_01)", "ment_wave_03(sub_ment_02)", "ment_wave_03(main_ment!!!!!!!!!!!!!!!!)"]
-                  ]
+  $(".ment_wave p").each(function(index, item){
+    var indexNum = index+1;
+    setTimeout(function(){
+      console.log(indexNum)
+      //각각 핸들링 해주려면, 각각의 클래스명을 붙여야겠지?
+      $(item).addClass('ment' + indexNum);
+      
+      //나타나야겠지?
+      setTimeout(function(){
+        $(item).show(indexNum*2000);
+      },1000)
+
+      $(item).hide(indexNum * 1000);
+      //나타났으니까, 사라져야겠지?
+
+      // $(item).css("animation", "ment "+indexNum*5+"s 3.5s 1 ease-out alternate");
+    },index*5000)
+  })
+
+  
+  // $(".chatbot_area .mentarea p").css("animation", "ment 5s 3.5s 1 ease-out alternate")
+  
+});
+  
+  // var mentArray = [
+  //                   ["ment_wave_01(sub_ment_01)", "ment_wave_01(sub_ment_02)", "ment_wave_01(main_ment!!!!!!!!!!!!!!!!)"],
+  //                   ["ment_wave_02(sub_ment_01)", "ment_wave_02(sub_ment_02)", "ment_wave_02(main_ment!!!!!!!!!!!!!!!!)"],
+  //                   ["ment_wave_03(sub_ment_01)", "ment_wave_03(sub_ment_02)", "ment_wave_03(main_ment!!!!!!!!!!!!!!!!)"]
+  //                 ]
 
   // var mentarea = $(".mentarea").attr("class");
   // console.log();
   
-  for(var i=0; i<mentArray.length; i++){
-    for(var z=0; z<mentArray[i].length; z++){
+  // for(var i=0; i<mentArray.length; i++){
+  //   for(var z=0; z<mentArray[i].length; z++){
       
-    }
-  }
-
-});
-
-$(document).on("click", ".chatbotClose", function () {
-  $(".chatbotArea").css("display", "none");
-});
-
+  //   }
+  // }
 /*
   이미지 관련
   1. 이미지는 총 2장이다.(A, B)
